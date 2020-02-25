e2869c256ba9e634d04527103e361666b756fe63
paubarranca <paubarranca91@gmail.com>
2020-02-25 22:24:42 +0100

    Migrated cert-manager to new repo

--- networking & load balancing/TLS/certmanager/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Create k8s objects
-kubectl create namespace cert-manager
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.13.1/cert-manager.yaml
-kubectl create clusterrolebinding cluster-admin-binding \
-  --clusterrole=cluster-admin \
-  --user=$(gcloud config get-value core/account)
